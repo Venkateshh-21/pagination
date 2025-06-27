96c33b3be2cb1400c2f62f95293748f86ae1ba48
venkatesh-21 <rajavenkatesh610@gmail.com>
2025-06-27 21:54:20 +0530

    fixing code

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -4,42 +4,42 @@ import "./pagination.css";
 import React from "react";
 
 function Pagination() {
-    const [data,setData]= useState([])
-    const [currData,setCurrData]= useState([])
+  const [data, setData] = useState([]);
+  const [currData, setCurrData] = useState([]);
   const [page, SetPage] = useState(1);
-
-  useEffect(()=>{
-  const dataFetch=async()=>{
-      try {
-      let response= await fetch("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json")
-     let data= await response.json()
-     setData(data)
-    } catch (e) {
-        alert("failed to fetch data")
-    }
-  }
-  dataFetch()
-  },[])
- useEffect(()=>{
-    const start= (page-1) * 10
-    const end= start+10
-    setCurrData([...data].slice(start,end))
- },[data,page])
-
- const handlePrev=()=>{
-    const totalPages=data.length
-    console.log(data.length)
-    if(page>0 && page<totalPages){
-        SetPage(prev => prev-1)
-    }
- }
- const handleNext =()=>{
-    const totalPages=data.length
-    if(page>0 && page<totalPages){
-        SetPage(prev => prev+1)
-    }
-  }
   const totalPages = Math.ceil(data.length / 10);
+
+  useEffect(() => {
+    const dataFetch = async () => {
+      try {
+        let response = await fetch(
+          "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+        );
+        let data = await response.json();
+        setData(data);
+      } catch (e) {
+        alert("failed to fetch data");
+      }
+    };
+    dataFetch();
+  }, []);
+  useEffect(() => {
+    const start = (page - 1) * 10;
+    const end = start + 10;
+    setCurrData([...data].slice(start, end));
+  }, [data, page]);
+
+  const handlePrev = () => {
+    console.log(data.length);
+    if (page > 0) {
+      SetPage((prev) => prev - 1);
+    }
+  };
+  const handleNext = () => {
+    if (page < totalPages) {
+      SetPage((prev) => prev + 1);
+    }
+  };
 
   return (
     <div>
@@ -62,20 +62,30 @@ function Pagination() {
           </tr>
         </thead>
         <tbody className="tablebody">
-            {currData.map((a)=>{
-              return  <tr key={a.id} className="tablerowdata">
-                  <td>{a.id}</td>
-                  <td>{a.name}</td>
-                  <td>{a.email}</td>
-                  <td>{a.role}</td>
-                </tr>
-            })}
+          {currData.map((a) => {
+            return (
+              <tr key={a.id} className="tablerowdata">
+                <td>{a.id}</td>
+                <td>{a.name}</td>
+                <td>{a.email}</td>
+                <td>{a.role}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
       <div className="buttons">
-        <button className="previous" disabled={page==1} onClick={handlePrev}>Previous</button>
+        <button className="previous" disabled={page == 1} onClick={handlePrev}>
+          Previous
+        </button>
         <p className="para">{page}</p>
-        <button className="next" disabled={page==totalPages}onClick={handleNext}>Next</button>
+        <button
+          className="next"
+          disabled={page == totalPages}
+          onClick={handleNext}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
